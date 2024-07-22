c559a2c2de3dc30d3336bad58f38c0f65f696b0c
burak <byrkbrk@gmail.com>
2024-07-22 17:34:08 +0300

    add height & width args into parser

--- generate.py
+++ generate.py
@@ -10,9 +10,15 @@ def parse_arguments():
     parser.add_argument("--num_inference_steps", type=int, default=5, 
                         help="Number of inferences steps that be used during inference. Default: 5")
     parser.add_argument("--device", type=str, default=None, help="Device name used for inferece. Default: None")
+    parser.add_argument("--height", type=int, default=512, help="Height of the generated image. Default: 512")
+    parser.add_argument("--width", type=int, default=512, help="Width of the generated image. Default: 512")
     return parser.parse_args()
 
 
 if __name__ == "__main__":
     args = parse_arguments()
-    StableDiffusion(device=args.device).generate(args.prompt, args.num_inference_steps)
+    StableDiffusion(device=args.device).generate(
+        args.prompt, 
+        args.num_inference_steps,
+        height=args.height,
+        width=args.width)
